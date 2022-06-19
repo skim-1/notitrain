69dcecf2bda90002925487a622f0fbb5d424baf2
Sunjung Kim <sunny2004.kim@gmail.com>
2022-06-18 19:47:39 -0500

    upload

---
+++ test.py
@@ -0,0 +1,11 @@
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from heapq import nlargest
+
+def shorten(txt):
+    print('aneesh')
+
+
+txt = str(open('in.txt', 'r', encoding='utf-8').read())
+shorten(txt)
